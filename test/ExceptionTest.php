0088d091d56fc6f4ad5ec2e01e1a38a08fb40fb7
andela-iadeniyi <ibonly01@gmail.com>
2015-10-04 01:46:21 +0100

    [#104812512] Create TestCase class

---
+++ test/ExceptionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+*
+*/
+
+namespace Ibonly\GithubStatusEvangelist\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class ExceptionTest extends PHPUnit_Framework_TestCase
+{
+
+}
---
+++ test/UserStatusTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+*
+*/
+
+namespace Ibonly\GithubStatusEvangelist\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class UserStatusTest extends PHPUnit_Framework_TestCase
+{
+
+}
