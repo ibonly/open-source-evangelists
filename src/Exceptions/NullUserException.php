c97f74e91167ffa4f31caad0a7c4219845800af7
Adeniyi Ibraheem <ibonly01@gmail.com>
2015-10-06 16:17:12 +0100

    Modify NullUserException documentation

--- src/Exceptions/NullUserException.php
+++ src/Exceptions/NullUserException.php
@@ -1,8 +1,11 @@
 <?php
-
 /**
-*
-*/
+ * Exception for empty user input
+ *
+ * @package Ibonly\GithubStatusEvangelist\NullUserException
+ * @author  Ibraheem ADENIYI <ibonly01@gmail.com>
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
 
 namespace Ibonly\GithubStatusEvangelist;
 use Exception;
@@ -12,10 +15,16 @@ class NullUserException extends Exception
 
     protected $exception_message;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->exception_message = "Username is empty";
     }
 
+    /**
+     * Handle empty user
+     *
+     * @return string
+     */
     public function errorMessage()
     {
         return "Error: " . $this->exception_message;
