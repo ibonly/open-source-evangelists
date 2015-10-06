c8d2d9852f7c41c7c0736dd7c8c0034c79ad39f8
Adeniyi Ibraheem <ibonly01@gmail.com>
2015-10-06 15:53:04 +0100

    Modify UserStatusTest documentation

--- test/UserStatusTest.php
+++ test/UserStatusTest.php
@@ -1,8 +1,11 @@
 <?php
-
 /**
-*
-*/
+ * Run test on the UserStatusTest
+ *
+ * @package Ibonly\GithubStatusEvangelist\Test\UserStatusTest
+ * @author  Ibraheem ADENIYI <ibonly01@gmail.com>
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
 
 namespace Ibonly\GithubStatusEvangelist\Test;
 
@@ -11,26 +14,40 @@ use Ibonly\GithubStatusEvangelist\EvangelistStatus;
 
 class UserStatusTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Define class initialization
+     */
     public function setUp()
     {
         $this->evangelists = new EvangelistStatus('andela-iadeniyi');
     }
 
+    /**
+     * Test to check if Number of repositories return Integer
+     */
     public function testgetRepoNumberOutputIsInteger()
     {
         $this->assertEquals("integer", gettype($this->evangelists->getRepoNumber()));
     }
 
+    /**
+     * Test to check if Name return string
+     */
     public function testgetNameOutputIsInteger()
     {
         $this->assertEquals("string", gettype($this->evangelists->getName()));
     }
-
+    /**
+     * Test to check if follower return Integer
+     */
     public function testgetFollowersOutputIsInteger()
     {
         $this->assertEquals("integer", gettype($this->evangelists->getFollowers()));
     }
 
+    /**
+     * Test to check if Following return Integer
+     */
     public function testgetFollowingOutputIsInteger()
     {
         $this->assertEquals("integer", gettype($this->evangelists->getFollowing()));
