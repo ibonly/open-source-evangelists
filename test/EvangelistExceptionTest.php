964feca9ccf11d70cbd6a6c776fc5135ef9b9b62
Adeniyi Ibraheem <ibonly01@gmail.com>
2015-10-06 15:44:29 +0100

    Modify EvangelistExceptionTest documentation

--- test/EvangelistExceptionTest.php
+++ test/EvangelistExceptionTest.php
@@ -1,8 +1,12 @@
 <?php
-
 /**
-*
-*/
+ * Run test on the EvangelistRank class for both guzzlehhtp and NullUserException
+ *
+ * @package Ibonly\GithubStatusEvangelist\Test\EvangelistExceptionTest
+ * @author  Ibraheem ADENIYI <ibonly01@gmail.com>
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
+
 
 namespace Ibonly\GithubStatusEvangelist\Test;
 
@@ -33,7 +37,6 @@ class EvangelistExceptionTest extends PHPUnit_Framework_TestCase
     /**
      * Test if Github username does not exist
      */
-
     public function testInvalidUserException() {
         $evangelists = new EvangelistStatus('skfmfsdkds');
         $this->assertEquals("Invalid user", $evangelists->getStatus());
